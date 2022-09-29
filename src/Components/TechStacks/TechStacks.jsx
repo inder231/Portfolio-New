49c9114efebd59f1320e1831902fa5a359829dc2
unknown <inder39811@gmail.com>
2022-09-29 09:36:25 +0530

    new portfolio

---
+++ src/Components/TechStacks/TechStacks.jsx
@@ -0,0 +1,131 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../ContextProvider/ThemeContext";
+import styles from "./TechStacks.module.css";
+import {
+  DiJavascript1,
+  DiHtml5,
+  DiCss3,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiGit,
+} from "react-icons/di";
+import { SiRedux, SiExpress } from "react-icons/si";
+
+const TechStacks = () => {
+  const { newTheme } = useContext(ThemeContext);
+  return (
+    <div
+      data-aos="fade-right"
+      data-aos-offset="200"
+      data-aos-easing="ease-in-sine"
+      data-aos-duration="800"
+    >
+      <h1 style={{ color: `${newTheme.title}` }} className={styles.heading}>
+        Skills
+      </h1>
+      <div className={styles.borderBottom} />
+
+      <div className={styles.container}>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiHtml5 />
+          <span>HTML</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiCss3 />
+          <span>CSS</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiJavascript1 />
+          <span>Javascript</span>
+        </div>
+
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiReact />
+          <span>React</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <i style={{ color: "#7248B6" }} className="devicon-redux-original" />
+          <SiRedux />
+          <span>Redux</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiNodejs />
+          <span>Node</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <i className="devicon-express-original" />
+          <SiExpress />
+          <span>Express</span>
+        </div>
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiMongodb />
+          <span>MongoDB</span>
+        </div>
+
+        <div
+          style={{
+            color: `${newTheme.title}`,
+            background: `${newTheme.linkHover}`,
+            height: "100px",
+          }}
+          className={styles.logoWrapper}
+        >
+          <DiGit />
+
+          <span>Git</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStacks;
---
+++ src/Utils/Projects.js
@@ -0,0 +1,49 @@
+import bewakoof1 from "../Utils/images/bewakoof1.png";
+import sportjam1 from "../Utils/images/sportjam1.png";
+import tripsVilla from "../Utils/images/tripsVilla.png";
+
+export const projects = [
+  {
+    img: tripsVilla,
+    title: "TRIPSVILLA",
+    des: "Largest holiday home rental portal in Asia. Browse your ideal vacation home from over 100,000 holiday homes spread across destinations.",
+    demo: "",
+    live: "https://tripsvilla-clone-project.netlify.app/",
+    gitHub: "https://github.com/Inder-Pal-github/tripVilla",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "Redux",
+      "Chakra-UI",
+      "GitHub",
+    ],
+  },
+  {
+    img: bewakoof1,
+    title: "BEWAKOOF.COM",
+    des: "Bewakoof is an Online Shopping site for Men and Women Clothing. Shop from a wide range of T-shirts, Mobile Covers, Accessories and more at the best prices.",
+    demo: "",
+    live: "https://bewakoof-project.vercel.app/",
+    gitHub: "https://github.com/Inder-Pal-github/bewakoof-clone",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "Redux",
+      "Localstorage",
+      "GitHub",
+    ],
+  },
+  {
+    img: sportjam1,
+    title: "SPORTSJAM.IN",
+    des: "Buy Sports Goods Online India at best prices at Sportsjam.in India's leading Online Sports & Fitness Equipment Store. Enjoy shopping!",
+    demo: "",
+    live: "https://resonant-fudge-022d00.netlify.app/",
+    gitHub: "https://github.com/Inder-Pal-github/sportsman_clone",
+    technologies: ["HTML", "CSS", "JavaScript", "Localstorage", "GitHub"],
+  },
+];
