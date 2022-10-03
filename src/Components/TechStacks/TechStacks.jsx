725ac48f314c693c3adc6fcfa8c5cc980208b7b3
unknown <inder39811@gmail.com>
2022-10-03 12:50:06 +0530

    added different skills sections

--- src/Components/TechStacks/TechStacks.jsx
+++ src/Components/TechStacks/TechStacks.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../../ContextProvider/ThemeContext";
 import styles from "./TechStacks.module.css";
 import GitHubCalendar from "react-github-calendar";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import {
   DiJavascript1,
   DiHtml5,
@@ -11,7 +12,8 @@ import {
   DiMongodb,
   DiGit,
 } from "react-icons/di";
-import { SiRedux, SiExpress } from "react-icons/si";
+import { SiRedux, SiExpress, SiChakraui } from "react-icons/si";
+import { FcTreeStructure } from "react-icons/fc";
 
 const TechStacks = () => {
   const { newTheme } = useContext(ThemeContext);
@@ -26,106 +28,146 @@ const TechStacks = () => {
         Skills
       </h1>
       <div className={styles.borderBottom} />
+      <div className={styles.skills}>
+        <div className={styles.frontend}>
+          <div className={styles.techText}>frontend</div>
+          <div className={styles.container}>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiHtml5 />
+              <span>HTML</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiCss3 />
+              <span>CSS</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiJavascript1 />
+              <span>Javascript</span>
+            </div>
 
-      <div className={styles.container}>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiHtml5 />
-          <span>HTML</span>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiReact />
+              <span>React</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <i
+                style={{ color: "#7248B6" }}
+                className="devicon-redux-original"
+              />
+              <SiRedux />
+              <span>Redux</span>
+            </div>
+          </div>
         </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiCss3 />
-          <span>CSS</span>
+        <div className={styles.backend}>
+          <div className={styles.techText}>backend</div>
+          <div className={styles.container}>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiNodejs />
+              <span>Node</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <i className="devicon-express-original" />
+              <SiExpress />
+              <span>Express</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiMongodb />
+              <span>MongoDB</span>
+            </div>
+          </div>
         </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiJavascript1 />
-          <span>Javascript</span>
-        </div>
+        <div className={styles.others}>
+          <div className={styles.techText}>Others</div>
+          <div className={styles.container}>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+                height: "100px",
+              }}
+              className={styles.logoWrapper}
+            >
+              <FcTreeStructure />
 
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiReact />
-          <span>React</span>
-        </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <i style={{ color: "#7248B6" }} className="devicon-redux-original" />
-          <SiRedux />
-          <span>Redux</span>
-        </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiNodejs />
-          <span>Node</span>
-        </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <i className="devicon-express-original" />
-          <SiExpress />
-          <span>Express</span>
-        </div>
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiMongodb />
-          <span>MongoDB</span>
-        </div>
+              <span>DSA</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+                height: "100px",
+              }}
+              className={styles.logoWrapper}
+            >
+              <SiChakraui />
 
-        <div
-          style={{
-            color: `${newTheme.title}`,
-            background: `${newTheme.linkHover}`,
-            height: "100px",
-          }}
-          className={styles.logoWrapper}
-        >
-          <DiGit />
+              <span>chakra-UI</span>
+            </div>
+            <div
+              style={{
+                color: `${newTheme.title}`,
+                background: `${newTheme.linkHover}`,
+                height: "100px",
+              }}
+              className={styles.logoWrapper}
+            >
+              <DiGit />
 
-          <span>Git</span>
+              <span>Git</span>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.githubcalender} >
+      <div className={styles.githubcalender}>
         <GitHubCalendar
           username="Inder-Pal-github"
           year={new Date().getFullYear()}
